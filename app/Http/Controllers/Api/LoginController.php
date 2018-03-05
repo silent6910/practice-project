f6958b493ed56a8b107b4de0afce5aae77275a7c
silentJob <silent@jetfuel.com.tw>
2018-03-05 18:14:15 +0800

    finish jwt token auth,socialite,exception handler and response format

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\UserRepository;
+use App\Service\JwtService;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @param $provider
+     * @param UserRepository $userRepository
+     * @param JwtService $jwtService
+     * @return string
+     */
+    public function handleProviderCallback($provider, UserRepository $userRepository, JwtService $jwtService)
+    {
+        try {
+            $user = Socialite::driver($provider)->stateless()->user();
+
+            $user = $userRepository->socialiteUser($user->getId(), $provider, $user->getEmail(), $user->getName());
+
+            $jwtToken = $jwtService->authenticate($user);
+
+
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage());
+        }
+
+        return $jwtToken;
+    }
+
+    public function logout(JwtService $jwtService)
+    {
+        $jwtService->invalidateToken();
+        return response()->json(['success' => 'logout success'], 200);
+    }
+}
--- config/httpStatus.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'defaultStatus' => 200,
-    'successStats' => 200,
-];
