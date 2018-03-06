27b43bc7cce640d30d190ea86fad2922206ed4a8
silentJob <silent@jetfuel.com.tw>
2018-03-06 18:02:37 +0800

    patch create user initial role

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -4,6 +4,7 @@
 namespace App\Repository;
 
 use App\Model\User;
+use Illuminate\Support\Facades\DB;
 
 class UserRepository
 {
@@ -14,16 +15,37 @@ class UserRepository
         $this->model = $user;
     }
 
-    public function socialiteUser(string $providerId, string $provider, string $email, ?string $name)
+    /**
+     * @param string $providerId
+     * @param string $provider
+     * @param string $email
+     * @param string $name
+     * @return User $user
+     */
+    public function socialiteUser(string $providerId, string $provider, string $email, string $name)
     {
-        return $this->model->firstOrCreate(
-            [
-                'email' => $email,
-                'name' => $name,
-                'provider_id' => $providerId,
-                'provider' => $provider
-            ]
-        );
+        return DB::transaction(function () use ($providerId, $provider, $email, $name) {
+            $user = $this->model->firstOrCreate(
+                [
+                    'email' => $email,
+                    'name' => $name,
+                    'provider_id' => $providerId,
+                    'provider' => $provider
+                ]
+            );
+            $user = $this->giveDefRole($user);
+            return $user;
+        });
+
+    }
+
+    /**
+     * @param User $user
+     * @return User
+     */
+    public function giveDefRole(User $user)
+    {
+        return $user->assignRole(config('role.premiumRole'));
     }
 
 }
