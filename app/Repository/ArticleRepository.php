f8e639e4412a29d41273d569181bc2769fb4e8a6
silentJob <silent@jetfuel.com.tw>
2018-03-12 19:05:02 +0800

    add article action first edition

--- app/Repository/ArticleRepository.php
+++ app/Repository/ArticleRepository.php
@@ -3,8 +3,68 @@
 
 namespace App\Repository;
 
+use App\Model\Article;
 
 class ArticleRepository
 {
+    /**
+     * @var Article
+     */
+    private $model;
+
+    /**
+     * ArticleRepository constructor.
+     * @param Article $article
+     */
+    public function __construct(Article $article)
+    {
+        $this->model = $article;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIndex()
+    {
+        return $this->model->select('id', 'title', 'created_at', 'updated_at')->get();
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function getDetail(int $id)
+    {
+        return $this->model->select($this->model::COLUMN)->find($id);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function store(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    /**
+     * @param Article $article
+     * @param array $data
+     * @return bool
+     */
+    public function update(Article $article, array $data)
+    {
+        return $article->update($data);
+    }
+
+    /**
+     * @param Article $article
+     * @return bool|null
+     * @throws \Exception
+     */
+    public function destroy(Article $article)
+    {
+        return $article->delete();
+    }
 
 }
