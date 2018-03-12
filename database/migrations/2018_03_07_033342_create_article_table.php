d0827a995347179cd094a2b5d7c9e4eb91553753
silentJob <silent@jetfuel.com.tw>
2018-03-12 19:03:38 +0800

    finish article and comment migration and model

---
+++ database/migrations/2018_03_07_033342_create_article_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('type')->unsigned()->nullable(false)->default(0)->comment('article type');
+            $table->string('title', 300)->nullable(false)->default('')->comment('article title');
+            $table->text('content')->default('')->comment('article content');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
