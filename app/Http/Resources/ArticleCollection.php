e8ed05fc34a3bee88138ac5907ef8a8e081000ff
silentJob <silent@jetfuel.com.tw>
2018-03-26 18:55:18 +0800

    finish article list and logout

--- app/Http/Resources/ArticleCollection.php
+++ app/Http/Resources/ArticleCollection.php
@@ -4,16 +4,20 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class Users extends ResourceCollection
+class ArticleCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
+        $username = auth()->user()->name;
+        $this->collection->map(function ($item) use ($username) {
+            $item->isAuthor = ($item->author == $username);
+        });
         return parent::toArray($request);
     }
 }
