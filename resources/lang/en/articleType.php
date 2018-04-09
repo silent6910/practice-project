a0df1c22410afd7b3fa091520e5a6a6b82afe61c
silentJob <silent@jetfuel.com.tw>
2018-04-09 11:27:35 +0800

    add article type localization

---
+++ resources/lang/en/articleType.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    ArticleType::BULLETIN => 'bulletin',
+    ArticleType::DISCUSS => 'discuss',
+    ArticleType::PROBLEM => 'problem',
+    ArticleType::EXPERIENCE => 'experience',
+];
